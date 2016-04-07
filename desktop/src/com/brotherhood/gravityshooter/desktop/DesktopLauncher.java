251d1becc596d003968cac01219e9f1149c58935
Wojciech Osak <wojtek33314a@o2.pl>
2016-04-07 23:29:43 +0200

    init
    project init

---
+++ desktop/src/com/brotherhood/gravityshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brotherhood.gravityshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brotherhood.gravityshooter.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
