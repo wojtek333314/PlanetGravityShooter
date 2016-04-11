b6791180d54b6f3765c7dd4c562787a448dc100f
Przemysław Kudła <przemyslawkudla94@gmail.com>
2016-04-11 22:40:49 +0200

    create a GameMenu class

--- core/src/com/brotherhood/gravityshooter/stages/GameMenu.java
+++ core/src/com/brotherhood/gravityshooter/stages/GameMenu.java
@@ -1,6 +1,8 @@
 package com.brotherhood.gravityshooter.stages;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.brotherhood.gravityshooter.utils.PhysicsStage;
 
 /**
@@ -10,15 +12,23 @@ public class GameMenu extends PhysicsStage {
     public static final float GRAVITY_CONSTANT = (float) (4.3f * Math.pow(10, -2));
 
 
-    public GameMenu()
-    {
-        super(0,0,true);
+    public GameMenu() {
+        super(0, 0, true);
         setDrawDebugLines(true);
-        setBackgroundColor(new Color(0,0,0,0));
+        setBackgroundColor(new Color(0, 0, 0, 0));
+        createBorder();
+
     }
 
     @Override
     public void onWorldStep() {
 
+    }
+
+    public void createBorder(){
+        Body leftBorder = createBox(0,0,0.01f,H, BodyDef.BodyType.StaticBody);
+        Body rightBorder = createBox(W,0,1,H, BodyDef.BodyType.StaticBody);
+        Body topBorder = createBox(0,H,W,1, BodyDef.BodyType.StaticBody);
+        Body bottomBorder = createBox(0,0,W,1, BodyDef.BodyType.StaticBody);
     }
 }
